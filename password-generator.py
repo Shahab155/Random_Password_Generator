c02643855cc7d5478950f4a58d16722d4fa1ae0b
shahab2015 <166250566+shahab2015@users.noreply.github.com>
2025-03-06 01:13:07 +0500

    first comment

---
+++ password-generator.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import random # Importing the random library for generating random characters
+import string # Import the string for using string characters
+
+def load_css():
+    with open("style.css","r") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
+def generate_password(length,use_digits,use_special):
+    characters = string.ascii_letters # provides uppercase and lowercase characters (a-z) (A-Z)
+
+    if use_digits:
+        characters += string.digits # provides number from  0 to 9
+
+    if use_special: 
+        characters += string.punctuation  # provides special characters 
+# This line generate the random passwor based on user preferences 
+    return ''.join(random.choice(characters) for _ in range(length))
+#  underscore(_)  tells python that loop does not have length(specified) 
+
+st.sidebar.title("💥Welcome to my new App")
+name = st.sidebar.text_input("Name: ")
+email = st.sidebar.text_input("Email: ")
+message = st.sidebar._text_area("Comments: ")
+
+if st.sidebar.button("Submit"):
+    st.sidebar.success("Your meesage is recieved!")
+
+
+st.title("🔒 Random Password Generator")
+
+length = st.slider("Select Password Length", min_value=4 , max_value=50, value=12) # value 12 for by default values
+
+use_digits = st.checkbox("Include Digits")
+
+use_special = st.checkbox("Include Special Characters")
+
+if st.button("Generate Password"):
+    password = generate_password(length, use_digits, use_special)
+    st.text_input("Generated Password: ", password, type="password")
+    st.success("Password generated Successfully✅")
+  
+    
+st.subheader("Build with 💖 by Shahab")
+
+
